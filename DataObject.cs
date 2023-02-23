9432c503b7af6677fde570696dabae7af4ef62b0
Patrick Silva <pfivel@gmail.com>
2023-02-22 23:33:01 -0700

    initial commit

---
+++ DataObject.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace GPU_Performance_Analysis_Visualization
+{
+    public class DataObject
+    {
+        public string? dispatch { get; set; }
+        public string? gpuID { get; set; }
+        public string? queueID { get; set; }
+        public string? queueIndex { get; set; }
+        public string? processID { get; set; }
+        public string? threadID { get; set; }
+        public string? grd { get; set; }
+        public string? wgr { get; set; }
+        public string? lds { get; set; }
+        public string? scr { get; set; }
+        public string? archVgpr { get; set; }
+        public string? accumVgpr { get; set; }
+        public string? sgpr { get; set; }
+        public string? waveSize { get; set; }
+        public string? sig { get; set; }
+        public string? obj { get; set; }
+        public string? kernelName { get; set; }
+        public string? startTime { get; set; }
+        public string? endTime { get; set; }
+        public string? sqWaitAny { get; set; }
+        public string? sqWaveCycles { get; set; }
+        public string? sqCycles { get; set; }
+        public string? GRBMCount { get; set; }
+        public string? GRBMGUIActive { get; set; }
+
+
+        public void print()
+        {
+
+            Console.WriteLine("Dispatch: " + this.dispatch);
+            Console.WriteLine("GPU ID: " + this.gpuID);
+            Console.WriteLine("Queue ID: " + this.queueID);
+            Console.WriteLine("Queue Index: " + this.queueIndex);
+            Console.WriteLine("Process ID: " + this.processID);
+            Console.WriteLine("Thread ID: " + this.threadID);
+            Console.WriteLine("GRD: " + this.grd);
+            Console.WriteLine("WGR: " + this.wgr);
+            Console.WriteLine("LDS: " + this.lds);
+            Console.WriteLine("SCR: " + this.scr);
+            Console.WriteLine("Arch VGPR: " + this.archVgpr);
+            Console.WriteLine("ACCUM VGPR: " + this.accumVgpr);
+            Console.WriteLine("SGPR: " + this.sgpr);
+            Console.WriteLine("Wave Size: " + this.waveSize);
+            Console.WriteLine("SIG: " + this.sig);
+            Console.WriteLine("OBJ: " + this.obj);
+            Console.WriteLine("Kernel Name: " + this.kernelName);
+            Console.WriteLine("Start Time: " + this.startTime);
+            Console.WriteLine("End Time: " + this.endTime);
+            Console.WriteLine("SQ Wait Any: " + this.sqWaitAny);
+            Console.WriteLine("SQ Wave Cycles: " + this.sqWaveCycles);
+            Console.WriteLine("SQ Cycles: " + this.sqCycles);
+            Console.WriteLine("GRBM Count: " + this.GRBMCount);
+            Console.WriteLine("GRBM GUI Active: " + this.GRBMGUIActive);
+
+        }
+    }
+}
+
---
+++ ProcessData.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace GPU_Performance_Analysis_Visualization
+{
+    public class ProcessData
+    {
+        DataObject dataObj = new DataObject();
+
+        public void ProcessDataObject()
+        {
+            
+
+            string data = OpenRead();
+
+            string[] parsedData = data.Split(',');
+
+            dataObj.dispatch = parsedData[0];
+            dataObj.gpuID = parsedData[1];
+            dataObj.queueID = parsedData[2];
+            dataObj.queueIndex = parsedData[3];
+            dataObj.processID = parsedData[4];
+            dataObj.threadID = parsedData[5];
+            dataObj.grd = parsedData[6];
+            dataObj.wgr = parsedData[7];
+            dataObj.lds = parsedData[8];
+            dataObj.scr = parsedData[9];
+            dataObj.archVgpr = parsedData[10];
+            dataObj.accumVgpr = parsedData[11];
+            dataObj.sgpr = parsedData[12];
+            dataObj.waveSize = parsedData[13];
+            dataObj.sig = parsedData[14];
+            dataObj.obj = parsedData[15];
+            dataObj.kernelName = parsedData[16];
+            dataObj.startTime = parsedData[17];
+            dataObj.endTime = parsedData[18];
+            dataObj.sqWaitAny = parsedData[19];
+            dataObj.sqWaveCycles = parsedData[20];
+            dataObj.sqCycles = parsedData[21];
+            dataObj.GRBMCount = parsedData[22];
+            dataObj.GRBMGUIActive = parsedData[23];
+
+        }
+
+        public string OpenRead()
+        {
+            try
+            {
+                string text = System.IO.File.ReadAllText("/Users/patricksilva/projects/GPUPerformanceAnalysisVisualization/data/288440_results.txt");
+                return text;
+            }
+            catch (Exception e)
+            {
+                Console.Write("Error: " + e);
+            }
+            return "Error";
+        }
+
+        public void printDataObject()
+        {
+            dataObj.print();
+        }
+    }
+}
+
